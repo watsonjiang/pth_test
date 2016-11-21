e83882b6b10e3b0c3f8be55c0e8c9453edd5077d
watson <watson.jiang@gmail.com>
2016-11-21 18:04:09 +0800

    update co_time

--- co/co_time.c
+++ co/co_time.c
@@ -1,29 +1,49 @@
 #include "co.h"
 #include <time.h>
 #include <pthread.h>
+#include <string.h>
 /*
  * a module implements timer service.
  */
 
+typedef struct co_timeout_entry_st * co_timeout_entry_t;
+struct co_timeout_entry_st {
+    co_timeout_entry_t next;
+    struct timespec timeout;
+    co_t coroutine;
+}
+
+struct co_timer_wheel_slot_st {
+    co_timeout_entry_t head;
+};
+typedef struct co_timer_wheel_slot_st * co_timer_wheel_slot_t;
+
 struct co_timer_st {
+    struct co_timer_wheel_slot_st wheel[60];
     pthread_mutex_t lock;
     pthread_cond_t  new_timeout_cond;
-    int new_timeout_cnt;
-
+    co_timeout_entry_t new_timeout_list_head;
+    co_sched_t sched;    //the scheduler to receive timeout event
 };
 
 typedef struct co_timer_st * co_timer_t;
 
 
-void co_timer_init(co_timer_t t, co_sched_t s)
+void co_timer_init(co_timer_t *t, co_sched_t s)
 {
-
+    co_timer_t tmp = (co_timer_t)malloc(sizeof(struct co_timer_st));
+    pthread_mutex_init(&tmp->lock, NULL);
+    pthread_cond_init(&tmp->new_timeout_cond, NULL);
+    tmp->new_timeout_list_head == NULL;
+    tmp->sched = s;
+    memset(&tmp->wheel[0], 0, 60 * sizeof(struct co_timer_wheel_slot_st));
+    *t = tmp;
 }
 
-void co_timer_schedule(co_timer_t t, struct timeval abs_timeout)
+void co_timer_schedule(co_timer_t t, struct timeval abs_timeout, co_t coroutine)
 {
     pthread_mutex_lock(&t->lock);
-    new_timeout_cnt += 1;     
+        
     pthread_mutex_unlockk(&t->lock);
 }
 
