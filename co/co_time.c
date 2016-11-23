90ad2ffd15d95027d28396afdd28544d692f6f45
watson <watson.jiang@gmail.com>
2016-11-23 14:23:02 +0800

    update co_time

--- co/co_time.c
+++ co/co_time.c
@@ -2,79 +2,130 @@
 #include <time.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 /*
  * a module implements timer service.
  */
 
-typedef struct co_timeout_entry_st * co_timeout_entry_t;
-struct co_timeout_entry_st {
-    co_timeout_entry_t next;
-    struct timespec timeout;
-    co_t coroutine;
-}
+typedef void (*timeout_callback_t)(void * arg);
 
-struct co_timer_wheel_slot_st {
-    co_timeout_entry_t head;
+typedef struct co_timer_entry_st * co_timer_entry_t;
+struct co_timer_entry_st {
+    co_timer_entry_t next;
+    struct timespec timeout;
+    timeout_callback_t cb;
+    void * cb_arg;
 };
-typedef struct co_timer_wheel_slot_st * co_timer_wheel_slot_t;
 
 struct co_timer_st {
-    struct co_timer_wheel_slot_st wheel[60];
-    pthread_mutex_t lock;
-    pthread_cond_t  new_timeout_cond;
-    co_timeout_entry_t new_timeout_list_head;
-    co_sched_t sched;    //the scheduler to receive timeout event
+    co_timer_entry_t timer_list;
+    pthread_mutex_t new_timer_lock;
+    pthread_cond_t  new_timer_cond;
+    co_timer_entry_t new_timer_list;
 };
 
 typedef struct co_timer_st * co_timer_t;
 
-
-void co_timer_init(co_timer_t *t, co_sched_t s)
+void co_timer_init(co_timer_t *t)
 {
     co_timer_t tmp = (co_timer_t)malloc(sizeof(struct co_timer_st));
-    pthread_mutex_init(&tmp->lock, NULL);
-    pthread_cond_init(&tmp->new_timeout_cond, NULL);
-    tmp->new_timeout_list_head == NULL;
-    tmp->sched = s;
-    memset(&tmp->wheel[0], 0, 60 * sizeof(struct co_timer_wheel_slot_st));
+    pthread_mutex_init(&tmp->new_timer_lock, NULL);
+    pthread_cond_init(&tmp->new_timer_cond, NULL);
+    tmp->new_timer_list == NULL;
     *t = tmp;
 }
 
-void co_timer_schedule(co_timer_t t, struct timeval abs_timeout, co_t coroutine)
+void co_timer_schedule(co_timer_t t, struct timespec abs_timeout, timeout_callback_t cb, void *cb_arg)
 {
-    pthread_mutex_lock(&t->lock);
-        
-    pthread_mutex_unlockk(&t->lock);
+    co_timer_entry_t entry = (co_timer_entry_t)malloc(sizeof(struct co_timer_entry_st));
+    entry->next = NULL;
+    entry->timeout = abs_timeout;
+    entry->cb = cb;
+    entry->cb_arg = cb_arg;
+    pthread_mutex_lock(&t->new_timer_lock);
+    entry->next = t->new_timer_list;
+    t->new_timer_list = entry;
+    pthread_cond_signal(&t->new_timer_cond); 
+    pthread_mutex_unlock(&t->new_timer_lock);
 }
 
-static struct timeval get_next_timeout()
+static int co_time_cmp(struct timespec *t1, struct timespec *t2)
 {
-   return NULL;
+    if (t1->tv_sec < t2->tv_sec) {
+        return -1;
+    }else if (t1->tv_sec > t2->tv_sec) {
+        return 1;
+    }else if (t1->tv_nsec < t2->tv_nsec) {
+        return -1;
+    }else if (t1->tv_nsec > t2->tv_nsec) {
+        return 1;
+    }
+
+    return 0;
 }
 
-static void emit_timeout()
+static void emit_timeout(co_timer_t t, struct timespec * now)
 {
+    co_timer_entry_t cur = NULL;
+    if(NULL == t->timer_list) {
+        return;
+    }
 
+    cur = t->timer_list;
+    while(co_time_cmp(&cur->timeout, now)<0) {
+        cur->cb(cur->cb_arg);
+        cur = cur->next; 
+    }
+}
+
+static void handle_new_timer(co_timer_t t)
+{
+    pthread_mutex_lock(&t->new_timer_lock);
+    while(NULL != t->new_timer_list) {
+        if(NULL == t->timer_list) {
+            t->timer_list = t->new_timer_list;
+            t->new_timer_list = t->new_timer_list->next;
+            t->timer_list->next = NULL;
+            break;
+        }
+        co_timer_entry_t e = NULL; 
+        for(e=t->timer_list; e!=NULL; e=e->next) {
+            if(co_time_cmp(&t->new_timer_list->timeout, &e->timeout)>=0) {
+                co_timer_entry_t tmp = t->new_timer_list;
+                t->new_timer_list = tmp->next;
+                tmp->next = e->next;
+                e->next = tmp;
+                break;
+            }
+        }
+    }
+    pthread_mutex_unlock(&t->new_timer_lock);
 }
 
 void _co_timer_loop(co_timer_t t)
 {
+    struct timespec now;
     while(1) {
-        struct timeval now;
-        struct timeval next_timeout = get_next_timeout();
-        gettimeofday(&now, NULL);
-        while( time_cmp(next_timeout, now) < 0) {
-             emit_timeout(next_timeout); 
+        clock_gettime(CLOCK_REALTIME, &now);
+        emit_timeout(t, &now);
+        pthread_mutex_lock(&t->new_timer_lock);
+        while(NULL == t->new_timer_list) {
+            if(NULL == t->timer_list) { //no timer, wait for new timer
+                pthread_cond_wait(&t->new_timer_cond, &t->new_timer_lock);
+            }else{
+                struct timespec *next_timeout;
+                int ret;
+                next_timeout = &t->timer_list->timeout;
+                ret = pthread_cond_timedwait(&t->new_timer_cond, &t->new_timer_lock, next_timeout);
+                if(ETIMEDOUT == ret) {
+                    break;
+                }
+            }
         }
-        pthread_mutex_lock(&t->lock);
-        int ret;
-        while(t->new_timeout_cnt <= 0) {
-            ret = pthread_cond_timedwait(&t->new_timeout_cond, &t->lock, next_timeout);
+        pthread_mutex_unlock(&t->new_timer_lock);
+        if(NULL!=t->new_timer_list) { //new timer comes
+            handle_new_timer(t);    
         }
-        pthread_mutex_unlock(&t->lock);
-        if (ret == ETIMEDOUT) {
-            emit_timeout(next_timeout);
-        }
- 
     }
 }
